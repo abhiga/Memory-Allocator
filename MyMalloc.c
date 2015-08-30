eff97e55d6f0b492840905c005d2ae13d1b5bd99
abhiga <agaurav@purdue.edu>
2015-08-29 22:19:45 -0400

    20 of 40

--- MyMalloc.c
+++ MyMalloc.c
@@ -225,8 +225,20 @@ void freeObject( void * ptr )
 	while(ptr <= hdr){
 		ptr = ptr -> _next;
 	}*/
-	struct ObjectHeader *temp = hdr;
-
+	struct ObjectHeader * nexthdr = (struct ObjectHeader*) ((char*)ftr + sizeof(struct ObjectFooter));
+	struct ObjectHeader *temphdr = NULL;
+	struct ObjectFooter *prevftr = (struct ObjectFooter*) ((char *) ftr - ftr->_objectSize);
+	if (prevftr > _memStart) {
+		if(prevftr -> _allocated == 0) {
+			temphdr = (struct ObjectHeader *) ((char*) prevftr - prevftr-> _objectSize + sizeof(struct ObjectFooter)); 
+			prevftr -> _objectSize = prevftr -> _objectSize + ftr -> _objectSize;
+			temphdr -> _objectSize = prevftr -> _objectSize;
+		}
+		else if (nexthdr -> _allocated == 0) {
+			
+		}
+	}	
+		
 	return;
 
 }
